d4e669e3970fc911b4cc872015c2644da7160dee
SangYun0922 <skwx50000@gmail.com>
2022-07-21 09:40:41 +0900

    cache StressTest 코드 업데이트

--- src/main/java/com/inspien/fb/svc/CustMstService.java
+++ src/main/java/com/inspien/fb/svc/CustMstService.java
@@ -19,7 +19,7 @@ public class CustMstService {
         this.custMstMapper = custMstMapper;
     }
 
-//    @Cacheable(value = "CustMst", key = "#OrgCd")
+    @Cacheable(value = "CustMst", key = "#OrgCd")
     //key를 #OrgCd로 설정하였는데 이럴경우 getData에서 받는 파라미터 id값대로 캐시데이터가 저장되어,
     // 추후 캐시데이터를 key값에 따라 부분 업데이트를 할 수 있다. 즉, CustMst라는 캐시테이블 안에 OrgCd를 키값으로 그에 해당되는 데이터가 value로 설정된다.
     public List<CustMst> getData(String OrgCd) {
