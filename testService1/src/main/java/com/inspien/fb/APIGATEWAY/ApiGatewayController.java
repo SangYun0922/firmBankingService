ec914751de66b581b9842b03ab0b625867fece73
SangYun0922 <skwx50000@gmail.com>
2022-07-25 22:31:32 +0900

    test-service1 추가

---
+++ testService1/src/main/java/com/inspien/fb/APIGATEWAY/ApiGatewayController.java
@@ -0,0 +1,13 @@
+package com.inspien.fb.APIGATEWAY;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiGatewayController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to the first service";
+    }
+}
