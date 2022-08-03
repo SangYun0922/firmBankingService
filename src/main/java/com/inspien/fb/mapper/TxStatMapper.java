dfd8d163afd7a256433c7c262077300075956c7d
SangYun0922 <skwx50000@gmail.com>
2022-08-03 17:09:23 +0900

    modify to MSA

--- src/main/java/com/inspien/fb/mapper/TxStatMapper.java
+++
@@ -1,9 +0,0 @@
-package com.inspien.fb.mapper;
-
-import com.inspien.fb.domain.TxStat;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TxStatMapper {
-    public boolean insertTxStat(TxStat txStat);
-}
--- src/main/java/com/inspien/fb/mapper/TxTraceMapper.java
+++
@@ -1,9 +0,0 @@
-package com.inspien.fb.mapper;
-
-import com.inspien.fb.domain.TxTrace;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TxTraceMapper {
-    public boolean insertOrUpdateTxTrace(TxTrace txTrace);
-}
